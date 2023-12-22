bcf637a08d4cc196efcd94c312a2d727f3816d75
Diogo Gomes <dgomes@ua.pt>
2023-12-22 00:56:19 +0000

    initial refactor

--- src/pypolestar/polestar_api.py
+++ src/pypolestar/polestar_api.py
@@ -3,13 +3,15 @@
 from datetime import datetime, timedelta
 import json
 import logging
+import aiohttp
 
 from urllib.parse import parse_qs, urlparse
 
 from .const import (
     CACHE_TIME,
 )
-
+from .exceptions import PolestarApiException
+from .auth import PolestarAuth
 
 POST_HEADER_JSON = {"Content-Type": "application/json"}
 
@@ -18,19 +20,21 @@ _LOGGER = logging.getLogger(__name__)
 
 class PolestarApi:
 
-    async def init(self, session, username: str, password: str):
+    def __init__(self, session, username: str, password: str):
         self.username = username
         self.password = password
-        await self.get_token()
-        if self.access_token is None:
-            return
+        self._session = session or aiohttp.ClientSession()
+        self.auth = PolestarAuth(self._session, self.username, self.password)
+
+    async def init(self):
+        await self.auth.get_token()
+
         result = await self.get_vehicle_data()
 
         # check if there are cars in the account
         if result['data']['getConsumerCarsV2'] is None or len(result['data']['getConsumerCarsV2']) == 0:
-            _LOGGER.exception("No cars found in account")
-            # throw new exception
-            raise Exception("No cars found in account")
+            _LOGGER.error("No cars found in account")
+            raise PolestarApiException("No cars found in account")
 
         self.cache_data['getConsumerCarsV2'] = {
             'data': result['data']['getConsumerCarsV2'][0], 'timestamp': datetime.now()}
@@ -39,105 +43,6 @@ class PolestarApi:
         self.vin = result['data']['getConsumerCarsV2'][0]['vin']
         self.id = self.vin[:8]
         self.name = "Polestar " + self.vin[-4:]
-
-    async def _get_resume_path(self):
-        # Get Resume Path
-        params = {
-            "response_type": "code",
-            "client_id": "polmystar",
-            "redirect_uri": "https://www.polestar.com/sign-in-callback"
-        }
-        result = await self._session.get("https://polestarid.eu.polestar.com/as/authorization.oauth2", params=params)
-        if result.status != 200:
-            _LOGGER.error(f"Error getting resume path {result.status}")
-            return
-        return result.real_url.raw_path_qs
-
-    async def _get_code(self) -> None:
-        resumePath = await self._get_resume_path()
-        parsed_url = urlparse(resumePath)
-        query_params = parse_qs(parsed_url.query)
-
-        # check if code is in query_params
-        if query_params.get('code'):
-            return query_params.get(('code'))[0]
-
-        # get the resumePath
-        if query_params.get('resumePath'):
-            resumePath = query_params.get(('resumePath'))[0]
-
-        if resumePath is None:
-            return
-
-        params = {
-            'client_id': 'polmystar'
-        }
-        data = {
-            'pf.username': self.username,
-            'pf.pass': self.password
-        }
-        result = await self._session.post(
-            f"https://polestarid.eu.polestar.com/as/{resumePath}/resume/as/authorization.ping",
-            params=params,
-            data=data
-        )
-        self.latest_call_code = result.status
-        if result.status != 200:
-            _LOGGER.error(f"Error getting code {result.status}")
-            return
-        # get the realUrl
-        url = result.url
-
-        parsed_url = urlparse(result.real_url.raw_path_qs)
-        query_params = parse_qs(parsed_url.query)
-
-        if not query_params.get('code'):
-            _LOGGER.error(f"Error getting code in {query_params}")
-            _LOGGER.warning("Check if username and password are correct")
-            return
-
-        code = query_params.get(('code'))[0]
-
-        # sign-in-callback
-        result = await self._session.get("https://www.polestar.com/sign-in-callback?code=" + code)
-        self.latest_call_code = result.status
-        if result.status != 200:
-            _LOGGER.error(f"Error getting code callback {result.status}")
-            return
-        # url encode the code
-        result = await self._session.get(url)
-        self.latest_call_code = result.status
-
-        return code
-
-    async def get_token(self) -> None:
-        code = await self._get_code()
-        if code is None:
-            return
-
-        # get token
-        params = {
-            "query": "query getAuthToken($code: String!) { getAuthToken(code: $code) { id_token access_token refresh_token expires_in }}",
-            "operationName": "getAuthToken",
-            "variables": json.dumps({"code": code})
-        }
-
-        headers = {
-            "Content-Type": "application/json"
-        }
-        result = await self._session.get("https://pc-api.polestar.com/eu-north-1/auth/", params=params, headers=headers)
-        self.latest_call_code = result.status
-        if result.status != 200:
-            _LOGGER.error(f"Error getting token {result.status}")
-            return
-        resultData = await result.json()
-        _LOGGER.debug(resultData)
-
-        self.access_token = resultData['data']['getAuthToken']['access_token']
-        self.refresh_token = resultData['data']['getAuthToken']['refresh_token']
-        # ID Token
-
-        _LOGGER.debug(f"Response {self.access_token}")
 
     def get_latest_data(self, query: str, field_name: str) -> dict or bool or None:
         if self.cache_data and self.cache_data[query]:
@@ -205,13 +110,9 @@ class PolestarApi:
         self.updating = False
 
     async def get_graph_ql(self, params: dict):
-        headers = {
-            "Content-Type": "application/json",
-            "authorization": "Bearer " + self.access_token
-        }
+        headers = await self.auth.headers()
 
         result = await self._session.get("https://pc-api.polestar.com/eu-north-1/my-star/", params=params, headers=headers)
-        self.latest_call_code = result.status
         resultData = await result.json()
 
         # if auth error, get new token
@@ -222,7 +123,6 @@ class PolestarApi:
             else:
                 # log the error
                 _LOGGER.warning(resultData.get('errors'))
-                self.latest_call_code = 500  # set internal error
         _LOGGER.debug(resultData)
         return resultData
 
